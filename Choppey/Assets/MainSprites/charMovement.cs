d909e21dbcdd699c8a8c20a80a97bdcebe7dd53f
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2021-01-10 13:35:16 -1000

    turning mechanism works

--- Choppey/Assets/MainSprites/charMovement.cs
+++ Choppey/Assets/MainSprites/charMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class charMovement : MonoBehaviour
 {
+    private Vector3 mousePos;
+    private Vector3 charPos;
+    private Vector3 faceAngle;
+    public Camera daCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        charPos = this.transform.localPosition;
+        var mPos = Input.mousePosition;
+        mPos.z = this.transform.position.z - daCamera.transform.position.z;
+        mousePos = Camera.main.ScreenToWorldPoint(mPos);
+        faceAngle = mousePos - charPos;
+        Debug.Log(mousePos);
+        faceAngle = Vector3.Normalize(faceAngle);
+        //Debug.Log(faceAngle);
+        //Debug.Log(Mathf.Rad2Deg * Mathf.Atan(faceAngle.x / faceAngle.y));
+        if (faceAngle.y >= 0)
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -(Mathf.Rad2Deg * Mathf.Atan(faceAngle.x / faceAngle.y)));
+        else
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180 - (Mathf.Rad2Deg * Mathf.Atan(faceAngle.x / faceAngle.y)));
     }
 }
